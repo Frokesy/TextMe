971b9ceb7763127ef475aa1247c8642cdb5b0c95
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-09-14 03:56:06 +0100

    added a bit of animation

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,42 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import Login from './login'
 
-const index = () => {
+const Home = () => {
+  const [visible, setVisible] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(true)
+    }, 5000);
+  }, [])
   return (
     <div className="relative">
-      <div className="absolute left-10">
-        <Login />
-      </div>
-      {/* <motion.div
-      animate={{
-        opacity: [0, 1, 0, 1, 0, 1, 0],
-        scale: [1, 0.5, 1, 0.5, 1, 0.5, 2]
-      }}
-      transition={{
-        duration: 5,
-      }}
-      className="h-[90vh] flex justify-center items-center">
-        <Image src="/assets/messenger-green-icon.png" alt="logo" width="100px" height="100px" />
-      </motion.div> */}
+      {visible ? (
+         <motion.div 
+         className="absolute left-10"
+         initial={{ opacity: 0, scale: 2 }}
+         animate={{ opacity: 1, scale: 1 }}
+         transition={{ duration: 1 }}
+         >
+         <Login />
+       </motion.div>
+      ) : (
+        <motion.div
+        animate={{
+          opacity: [0, 1, 0, 1, 0, 1, 0],
+          scale: [1, 0.5, 1, 0.5, 1, 0.5, 2]
+        }}
+        transition={{
+          duration: 5,
+        }}
+        className="h-[90vh] flex justify-center items-center">
+          <Image src="/assets/messenger-green-icon.png" alt="logo" width="100px" height="100px" />
+        </motion.div>
+      )}
     </div>
   )
 }
 
-export default index
+export default Home
