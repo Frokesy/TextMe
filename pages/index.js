8a488edcfaa14850fee482a90698388347367c91
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-09-14 04:02:59 +0100

    made some minor adjustments

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,8 @@ const Home = () => {
     <div className="relative">
       {visible ? (
          <motion.div 
-         className="absolute left-10"
-         initial={{ opacity: 0, scale: 2 }}
+         className="absolute left-6"
+         initial={{ opacity: 0, scale: 1.5 }}
          animate={{ opacity: 1, scale: 1 }}
          transition={{ duration: 1 }}
          >
