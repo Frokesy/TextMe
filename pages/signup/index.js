e257659ffb8b519a1b02513a1f5375a99033b070
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-09-15 14:35:40 +0100

    added signup component

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import { motion } from 'framer-motion'
 
-const Login = () => {
+const Signup = () => {
   return (
     <motion.div 
     className="w-[85vw] mx-auto pt-[3vh]"
@@ -12,10 +12,30 @@ const Login = () => {
     >
             <h2 className="text-[#0fa84e] font-[Combo] font-semibold text-[28px]">TextMe</h2>
             <div className="text-[#ccc] mt-[6vh]">
-                <h2 className="text-[24px]">Login</h2>
-                <span className="text-gray-500 italic">Hello Champ! Please proceed to login.</span>
+                <h2 className="text-[24px]">Create an Account</h2>
+                <span className="text-gray-500 italic">Welcome here Champ, let&apos;s get you started!</span>
                 <div className="flex flex-col">
                     <form>
+                        <div className="input-field flex flex-col space-y-2 mt-6">
+                            <label htmlFor="name" 
+                                className="text-[#ccc]">
+                                    Name:
+                            </label>
+                            <input type="name" name="name" id="name" 
+                                placeholder="John Doe" 
+                                className="border-gray-500 border-2 rounded-md p-3 focus:border focus:border-[#ccc] placeholder:italic outline-none outline-offset-2"
+                            />
+                        </div>
+                        <div className="input-field flex flex-col space-y-2 mt-6">
+                            <label htmlFor="username" 
+                                className="text-[#ccc]">
+                                    Username:
+                            </label>
+                            <input type="text" name="username" id="username" 
+                                placeholder="johndoe19" 
+                                className="border-gray-500 border-2 rounded-md p-3 focus:border focus:border-[#ccc] placeholder:italic outline-none outline-offset-2"
+                            />
+                        </div>
                         <div className="input-field flex flex-col space-y-2 mt-6">
                             <label htmlFor="email" 
                                 className="text-[#ccc]">
@@ -37,22 +57,17 @@ const Login = () => {
                             />
                         </div>
                         <div className="flex items-center justify-between mt-8">
-                            <Link href="#" passHref>
-                                <span className="text-[#ccc] cursor-pointer text-[12px]">
-                                    Forgot Password?
-                                </span>
-                            </Link>
-                            <button className="bg-[#0fa84e] text-white font-semibold py-2 px-14 rounded-md">Login</button>
+                            <div className="">
+                                <span className="text-gray-500 text-[12px]">Already a user?</span>
+                                <Link href="/login" passHref>
+                                    <span className="text-[#0fa84e] cursor-pointer mx-2 text-[12px]">
+                                        Login
+                                    </span>
+                                </Link>
+                            </div>
+                            <button className="bg-[#0fa84e] text-white font-semibold py-2 px-14 rounded-md">Sign Up</button>
                         </div>
                     </form>
-                    <div className="flex justify-end pt-10 items-center">
-                        <span className="text-gray-500 text-[12px]">Don&apos;t have an account?</span>
-                        <Link href="/signup" passHref>
-                            <span className="text-[#0fa84e] cursor-pointer mx-2 text-[12px]">
-                                Let&apos;s get started
-                            </span>
-                        </Link>
-                    </div>
                 </div>
             </div>
 
@@ -64,4 +79,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Signup
