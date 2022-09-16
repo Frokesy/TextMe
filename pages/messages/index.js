d73293a88ee4d31e9bae7b1fbb2b30801b73a0c5
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-09-16 16:46:32 +0100

    modified the design on the messages component

--- pages/messages/index.js
+++ pages/messages/index.js
@@ -5,19 +5,19 @@ import { FaEdit, FaSearch } from 'react-icons/fa'
 const Messages = () => {
   return (
     <div>
-      <div className="w-[90vw] items-center mx-auto pt-6 flex justify-center">
-        <Image src="/assets/UserPP.svg" alt="logo" width="40px" height="40px" />
+      <div className="w-[90vw] items-center mx-auto pt-3 flex justify-center">
+        <Image src="/assets/UserPP.svg" alt="logo" width="60px" height="60px" />
       </div>
-        <div className="w-[90vw] mx-auto pt-10 flex items-center text-center justify-between">
-            <h1 className="text-[#ccc] text-[25px]">Messages</h1>
+        <div className="w-[90vw] mx-auto flex items-center text-center justify-between">
+            <h1 className="text-neutral-400 text-[30px]">Messages</h1>
             <FaEdit className="text-[#0fa84e] text-[22px]"/>
         </div>
-        <div className="w-[90vw] mx-auto mt-10 border border-gray-500 space-x-2 bg-gray-800 items-center rounded-2xl p-2 text-[#ccc] flex text-[13px]">
+        <div className="w-[90vw] mx-auto mt-2 space-x-2 bg-neutral-800 items-center rounded-2xl p-3 offset-0 border-none text-[#ccc] flex text-[13px]">
             <FaSearch className="bg-transparent"/>
-            <input type="text" placeholder="Search" className="w-full bg-transparent"/>
+            <input type="text" placeholder="Search" className="w-full h-full hover:border-none focus:border-none bg-transparent offset-0 outline-none border-none"/>
         </div>
       <div className="h-[70vh] w-screen text-center flex items-center justify-center">
-        <h1 className="text-gray-500 text-[14px] font-bold">No messages yet, start a conversation today!</h1>
+        <h1 className="text-gray-500 text-[11px] font-light">No messages yet, start a conversation today!</h1>
       </div>
     </div>
   )
