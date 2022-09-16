338c97c478657421ea3b2ee436f58d248c43bab1
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-09-16 02:29:31 +0100

    made supabase key public temporarily

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://sfkajewoyefbteavxhiw.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNma2FqZXdveWVmYnRlYXZ4aGl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMyODQ0OTksImV4cCI6MTk3ODg2MDQ5OX0.Joz2JjShMatKM_hnjlO3_mRVVXXgdxykX1E6NlasF0s'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
