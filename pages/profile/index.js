1b295f608b141b0a40972bbc8fc0fdd0ac373688
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-09-18 04:35:34 +0100

    users can now update profile basic information

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -9,6 +9,31 @@ import { data } from 'autoprefixer';
 
 const Profile = () => {
   const [profile, setProfile] = React.useState(null)
+  const [editData, setEditData] = React.useState({
+    basicInfo: false,
+    additionalInfo: false,
+    links: false,
+    password: false,
+  })
+  const [loading, setLoading] = React.useState({
+    default: true,
+    basicInfo: false,
+    additionalInfo: false,
+    links: false,
+    password: false,
+  })
+  const [message, setMessage] = React.useState({
+    error: '',
+    success: '',
+  })
+  const [update, setUpdate] = React.useState({
+    name: '',
+    username: '',
+    email: '',
+    mobile: '',
+    summary: '',
+  })
+
   const updatePic = (pic) => {
       console.log(pic)
   }
@@ -22,19 +47,75 @@ const Profile = () => {
         console.log(error)
     } else {
         setProfile(data[0])
-        console.log(data)
     }
   }
+
+  const updateBasicInfo = async (e) => {
+    e.preventDefault()
+    setLoading({
+      ...loading,
+      basicInfo: true,  
+    })
+    const name = update.name ? update.name : profile.name
+    const username = update.username ? update.username : profile.username
+    const { data, error } = await supabase
+        .from('profiles')
+        .update({ name, username })
+        .eq('user_id', supabase.auth.user().id)
+    if (error) {
+        console.log(error)
+        setMessage({
+            error: error.message,
+            success: '',
+        })
+    } else {
+        setProfile(data[0])
+        setMessage({
+            error: '',
+            success: 'Profile updated successfully',
+        })
+        setTimeout(() => {
+            setMessage({
+                error: '',
+                success: '',
+            })
+        }, 2000)
+        setEditData({
+            ...editData,
+            basicInfo: false,
+        })
+        setLoading({
+          ...loading,
+          basicInfo: false,
+      })
+    }
+
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+        setLoading({
+            ...loading,
+            default: false,
+        })
+    }, 1000)
+  }, [loading])
 
   useEffect(() => {
     getUser()
   }, [])
-  console.log(profile?.name)
+
 
   return (
-    <div className="pt-[5vh]">
+    <div>
+      {loading.default ? (
+        <div className=" h-screen w-screen opacity-75 absolute flex items-center justify-center">
+        <Spinner color="#0fa84e" size="lg" thickness="3px" />
+      </div>
+      ) : (
+        <div className="pt-[5vh]">
       <div className="w-min aspect-square relative mx-auto">
-        <Avatar size="2xl" mx="auto" name="DP" src="/assets/sub.jpg" />
+        <Avatar size="2xl" mx="auto" name={profile?.name} src="/assets/sub.jpg" />
         <div className="absolute text-[#fff] right-[10%] top-[80%] w-5 aspect-square border-[3px] rounded-full">
             <input accept="image/*" id="icon-button-file" type="file" style={{ display: 'none' }}  onChange={(e) => updatePic( e.target.files[0])} />
             <label htmlFor="icon-button-file" className="cursor-pointer">
@@ -42,55 +123,146 @@ const Profile = () => {
           </label>
           </div>    
         </div>
+
+
       <div className="w-[90vw] bg-neutral-800 rounded-lg shadow-md py-4 px-4 flex flex-col mx-auto mt-10">
-        <span className="text-neutral-400 text-[18px] font-semibold bg-transparent">Basic Info</span>
-        <div className="flex flex-col mt-4 bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent">Name</span>
-          <span className="text-neutral-200 text-[14px] bg-transparent">{profile?.name}</span>
+        {editData.basicInfo ? (
+          <div className="flex flex-col bg-transparent">
+            <span className="text-neutral-400 text-[18px] font-semibold bg-transparent">Basic Info</span>
+            <form className="bg-transparent" onSubmit={updateBasicInfo}>
+              <div className="flex flex-col mt-4 bg-transparent">
+                <label className="text-neutral-400 text-[13px] font-semibold bg-transparent">Name</label>
+                <input type="text" 
+                className="bg-transparent border-b-[1px] border-neutral-400 text-neutral-400 text-[13px] font-semibold focus:border-b-[1px] offset-0 outline-none hover:border-b-[1px]" 
+                placeholder={profile.name}
+                value={update.name}
+                onChange={(e) => setUpdate({ ...update, name: e.target.value })}
+                />
+              </div>
+              <div className="flex flex-col mt-4 bg-transparent">
+                <label className="text-neutral-400 text-[13px] font-semibold bg-transparent">Username</label>
+                <input type="text" 
+                className="bg-transparent border-b-[1px] border-neutral-400 text-neutral-400 text-[13px] font-semibold focus:border-b-[1px] offset-0 outline-none hover:border-b-[1px]" 
+                placeholder={profile.username}
+                value={update.username}
+                onChange={(e) => setUpdate({ ...update, username: e.target.value })}
+                />
+              </div>
+              <div className="flex bg-transparent flex-row justify-end mt-4">
+                <button 
+                  className="bg-[#0fa84e] text-[#fff] px-4 py-2 rounded-lg text-[12px] font-semibold">
+                    
+                      Save
+                </button>
+              </div>
+            </form>
+          </div>
+        ) : (
+        <div className="bg-transparent">
+          <div className="flex justify-between bg-transparent">
+            <span className="text-neutral-400 text-[18px] font-semibold bg-transparent">Basic Info</span>
+            <span onClick={() => setEditData({...editData, basicInfo: true})} className="text-[#0fa84e] cursor-pointer text-[15px] font-semibold bg-transparent">Edit</span>
+          </div>
+          {message ? 
+          (
+                <p 
+                className={`${ message.error && 'text-red-700' } ${ message.success && 'text-[#0fa84e]' } text-[10px] bg-transparent font-semibold mt-4 rounded-md text-center`}>
+                    { message.error ? message.error : message.success }
+                </p>
+          ) : ('')}
+
+          <div className="flex flex-col mt-4 bg-transparent">
+            <span className="text-neutral-300 text-[13px] font-semibold bg-transparent">Name</span>
+            <span className="text-neutral-400 text-[14px] bg-transparent">{profile?.name}</span>
+          </div>
+          <hr className="my-4" />
+          <div className="flex flex-col bg-transparent">
+            <span className="text-neutral-300 text-[13px] font-semibold bg-transparent">Username</span>
+            <span className="text-neutral-400 italic text-[14px] bg-transparent"> 
+              <span className="text-[#0fa84e] bg-transparent">@</span> 
+              {profile?.username}
+            </span>
+          </div>
         </div>
-        <hr className="my-4" />
-        <div className="flex flex-col bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent">Username</span>
-          <span className="text-neutral-200 italic text-[14px] bg-transparent"> 
-            <span className="text-[#0fa84e] bg-transparent">@</span> 
-            {profile?.username}
-          </span>
-        </div>
+        )}
       </div>
 
 
       <div className="w-[90vw] bg-neutral-800 rounded-lg shadow-md py-4 px-4 flex flex-col mx-auto mt-6">
-        <span className="text-neutral-400 text-[18px] font-semibold bg-transparent">Additional Info</span>
-        <div className="flex flex-col mt-4 bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent">Email</span>
-          <span className="text-neutral-200 text-[14px] bg-transparent">{profile?.email}</span>
+        {editData.additionalInfo ? (
+              <div className="flex flex-col bg-transparent">
+              <span className="text-neutral-400 text-[18px] font-semibold bg-transparent">Basic Info</span>
+              <form className="bg-transparent" onSubmit={updateBasicInfo}>
+                <div className="flex flex-col mt-4 bg-transparent">
+                  <label className="text-neutral-400 text-[13px] font-semibold bg-transparent">Name</label>
+                  <input type="text" 
+                  className="bg-transparent border-b-[1px] border-neutral-400 text-neutral-400 text-[13px] font-semibold focus:border-b-[1px] offset-0 outline-none hover:border-b-[1px]" 
+                  placeholder={profile.name}
+                  value={update.name}
+                  onChange={(e) => setUpdate({ ...update, name: e.target.value })}
+                  />
+                </div>
+                <div className="flex flex-col mt-4 bg-transparent">
+                  <label className="text-neutral-400 text-[13px] font-semibold bg-transparent">Username</label>
+                  <input type="text" 
+                  className="bg-transparent border-b-[1px] border-neutral-400 text-neutral-400 text-[13px] font-semibold focus:border-b-[1px] offset-0 outline-none hover:border-b-[1px]" 
+                  placeholder={profile.username}
+                  value={update.username}
+                  onChange={(e) => setUpdate({ ...update, username: e.target.value })}
+                  />
+                </div>
+                <div className="flex bg-transparent flex-row justify-end mt-4">
+                  <button 
+                    className="bg-[#0fa84e] text-[#fff] px-4 py-2 rounded-lg text-[12px] font-semibold">
+                      
+                        Save
+                  </button>
+                </div>
+              </form>
+            </div> 
+        ) : (
+           <div className="bg-transparent">
+            <div className="flex justify-between bg-transparent">
+              <span className="text-neutral-400 text-[18px] font-semibold bg-transparent">Additional Info</span>
+              <span onClick={() => setEditData({...editData, additionalInfo: true})} className="text-[#0fa84e] cursor-pointer text-[15px] font-semibold bg-transparent">Edit</span>
+            </div>
+            <div className="flex flex-col mt-4 bg-transparent">
+              <span className="text-neutral-300 text-[13px] font-semibold bg-transparent">Email</span>
+              <span className="text-neutral-400 text-[14px] bg-transparent">{profile?.email}</span>
+            </div>
+            <hr className="my-4" />
+            <div className="flex flex-col bg-transparent">
+              <span className="text-neutral-300 text-[13px] font-semibold bg-transparent">Mobile</span>
+              <span className="text-neutral-400 text-[14px] bg-transparent">{profile?.mobile}</span>
+            </div>
+            <hr className="my-4" />
+            <div className="flex flex-col bg-transparent">
+              <span className="text-neutral-300 text-[13px] font-semibold bg-transparent">Profile Summary</span>
+              <span className="text-neutral-400 text-[14px] bg-transparent">{profile?.summary}</span>
         </div>
-        <hr className="my-4" />
-        <div className="flex flex-col bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent">Mobile</span>
-          <span className="text-neutral-200 text-[14px] bg-transparent">+2349157881431</span>
-        </div>
-        <hr className="my-4" />
-        <div className="flex flex-col bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent mb-2">Profile Summary</span>
-          <span className="text-neutral-200 text-[14px] bg-transparent">{profile?.summary}</span>
-        </div>
+           </div>
+        )}
       </div>
 
+      
+
       <div className="w-[90vw] bg-neutral-800 rounded-lg shadow-md py-4 px-4 flex flex-col mx-auto mt-6">
-        <span className="text-neutral-400 text-[18px] font-semibold bg-transparent">Useful Links</span>
+        <div className="flex justify-between bg-transparent">
+          <span className="text-neutral-400 text-[18px] font-semibold bg-transparent">Useful Links</span>
+          <span className="text-[#0fa84e] text-[15px] font-semibold bg-transparent">Edit</span>
+        </div>
         <div className="flex flex-col mt-4 bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent">GitHub</span>
+          <span className="text-neutral-300 text-[13px] bg-transparent">GitHub</span>
           <a href="#" className="text-blue-800 text-[14px] bg-transparent">www.github.com</a>
         </div>
         <hr className="my-4" />
         <div className="flex flex-col bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent">LinkedIn</span>
+          <span className="text-neutral-300 text-[13px] bg-transparent">LinkedIn</span>
           <a href="#" className="text-blue-800 text-[14px] bg-transparent">www.linkedin.com</a>
         </div>
         <hr className="my-4" />
         <div className="flex flex-col bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent mb-2">Facebook</span>
+          <span className="text-neutral-300 text-[13px] bg-transparent mb-2">Facebook</span>
           <a href="#" className="text-blue-800 text-[14px] bg-transparent">www.facebook.com</a>
         </div>
       </div>
@@ -98,17 +270,17 @@ const Profile = () => {
       <div className="w-[90vw] bg-neutral-800 rounded-lg shadow-md py-4 px-4 flex flex-col mx-auto mt-6">
         <span className="text-neutral-400 text-[18px] font-semibold bg-transparent">Change Password</span>
         <div className="flex flex-col mt-4 bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent">Current Password</span>
+          <span className="text-neutral-300 text-[13px] font-semibold bg-transparent">Current Password</span>
           <input type="password" className="text-neutral-200 mt-2 text-[14px] bg-transparent border-none focus:outline-none" />
         </div>
         <hr className="mb-4" />
         <div className="flex flex-col bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent">New Password</span>
+          <span className="text-neutral-300 text-[13px] font-semibold bg-transparent">New Password</span>
           <input type="password" className="text-neutral-200 mt-2 text-[14px] bg-transparent border-none focus:outline-none" />
         </div>
         <hr className="mb-4" />
         <div className="flex flex-col bg-transparent">
-          <span className="text-neutral-400 text-[13px] bg-transparent">Confirm Password</span>
+          <span className="text-neutral-300 text-[13px] font-semibold bg-transparent">Confirm Password</span>
           <input type="password" className="text-neutral-200 mt-2 text-[14px] bg-transparent border-none focus:outline-none" />
         </div>
         <hr />
@@ -124,6 +296,8 @@ const Profile = () => {
                 <span>V 1.0.0</span>
                 <span className="">Designed by Frokes</span>
       </div>
+    </div>
+        )}
     </div>
   )
 }
