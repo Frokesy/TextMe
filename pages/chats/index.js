f02c7cc965244087be007400aa23a4a15742ab41
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-09-20 07:31:13 +0100

    reverted changes

--- pages/chats/index.js
+++ pages/chats/index.js
@@ -30,15 +30,14 @@ const Messages = () => {
         exit={{ opacity: 0 }}
         transition={{ duration: 0.5 }}
         >
-
-          <div className="w-[90vw] mt-8 mx-auto flex items-center text-center justify-between">
-              <h1 className="text-neutral-400 text-[30px]">Messages</h1>
-              {/* <FaEdit className="text-[#0fa84e] cursor-pointer text-[22px]" onClick={() => router.push('/chats/new') } /> */}
-              <div className="">
+          <div className="w-[90vw] cursor-pointer items-center mx-auto pt-4 flex justify-center">
             <Link href="/profile" passHref>
               <Avatar size="lg" mx="auto" name={user?.name} src={user?.profile_pic} />
             </Link>
           </div>
+          <div className="w-[90vw] mx-auto flex items-center text-center justify-between">
+              <h1 className="text-neutral-400 text-[30px]">Messages</h1>
+              <FaEdit className="text-[#0fa84e] cursor-pointer text-[22px]" onClick={() => router.push('/chats/new') } />
           </div>
           <div className="w-[90vw] mx-auto mt-2 space-x-2 bg-neutral-800 items-center rounded-2xl p-3 offset-0 border-none text-[#ccc] flex text-[13px]">
               <FaSearch className="bg-transparent"/>
